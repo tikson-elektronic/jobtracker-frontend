35155492a15f1330abb4f4080cb3e9647b6e75fe
tikson <tiksonelektronic@gmail.com>
2025-06-12 15:52:40 -0700

    Minimal app for error isolation

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import React, { useRef, useEffect, useState } from "react";
-
-function App() {
-  return <h1>Testing: This should show up</h1>;
-}
-export default App;
