cfd74ac8d669b7160d78d2b464733fa3d4ec5a04
tikson <tiksonelektronic@gmail.com>
2025-06-12 14:55:24 -0700

    Test: Update App.js to verify deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "5rem" }}>
+      <h1>🚀 Tikson Job Tracker</h1>
+      <p>If you see this, deployment is working!</p>
     </div>
   );
 }
